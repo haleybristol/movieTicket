869169a3fe31b3c3b04bf0b3f8a031c5b7cb0273
Brianna Popson and Haley Bristol <bpopson@gmail.com>
2016-01-26 14:43:09 -0800

    create the basic html page

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,37 @@ function Ticket(movieName, movieTime, movieAge){
 
 Ticket.prototype.price = function(){
   if (this.movieAge >= 60) {
-    return ("$8.00");
+    return (8);
   } else if (this.movieAge <= 59 && this.movieAge > 12){
-  return ("$10.00");
+  return (10);
   } else {
-    return ("$7.00");
+    return (7);
   }
 }
 
 Ticket.prototype.matinee = function(){
   if (this.movieTime <= "4:00pm") {
-    return ("$1.00");
+    return (1);
   } else {
-    return ("$2.00");
+    return (2);
   }
 }
 
 Ticket.prototype.newRelease = function(){
   if (this.movieName === "The Revenant" || this.movieName === "Star Wars" ) {
-    return ("$2.00");
+    return (2);
   } else {
-    return ("$1.00");
+    return (1);
   }
 }
 
 Ticket.prototype.finalPrice = function(){
-  Ticket.price() + Ticket.matinee() + Ticket.newRelease();
+  var result = this.price() + this.matinee() + this.newRelease();
+  return result;
 }
+
+$(document).ready(function() {
+  var testTicket = new Ticket("The Revenant", "6:00pm", "18");
+  var finalPrice = testTicket.finalPrice();
+
+});
--- specs/specs.js
+++ specs/specs.js
@@ -8,22 +8,22 @@ describe('Ticket', function() {
 
   it("will assign price to an age", function() {
     var testTicket = new Ticket("Terminator", "6:00pm", "8");
-    expect(testTicket.price()).to.equal("$7.00");
+    expect(testTicket.price()).to.equal(7);
   });
 
   it("will assign price to a movie time", function() {
     var testTicket = new Ticket("Terminator", "6:00pm", "8");
-    expect(testTicket.matinee()).to.equal("$2.00");
+    expect(testTicket.matinee()).to.equal(2);
   });
 
   it("will assign price to a movie name", function() {
     var testTicket = new Ticket("The Revenant", "6:00pm", "18");
-    expect(testTicket.newRelease()).to.equal("$2.00");
+    expect(testTicket.newRelease()).to.equal(2);
   });
 
   it("will calculate a price based on selection", function() {
     var testTicket = new Ticket("The Revenant", "6:00pm", "18");
-    expect(testTicket.finalPrice()).to.equal("$14.00");
+    expect(testTicket.finalPrice()).to.equal(14);
   });
 
 });
